16e5b721a7419e2e093083736cbd82336f5a2744
Ian Millington <idmillington@googlemail.com>
2012-04-30 11:11:04 +0100

    Restructuring makefile.

---
+++ test/README.md
@@ -0,0 +1,5 @@
+# Test Suite for GCE
+
+The test suite runs using QUnit. Run the ./run.sh (or use another tool
+to serve the test directory) and goto http://localhost:8081 to see the
+test results.
---
+++ test/tests/test-datatypes.js
@@ -0,0 +1,30 @@
+(function($) {
+    $(document).ready(function(){
+
+        module("datatypes.js");
+
+        test("a basic test example", function() {
+            ok( true, "this test is fine" );
+            var value = "hello";
+            equal( value, "hello", "We expect value to be hello" );
+        });
+
+        test("first test within module", function() {
+            ok( true, "all pass" );
+        });
+
+        test("second test within module", function() {
+            ok( true, "all pass" );
+        });
+
+        module("Module B");
+
+        test("some other test", function() {
+            expect(2);
+            equal( true, false, "failing test" );
+            equal( true, true, "passing test" );
+        });
+
+    });
+
+})(jQuery);
